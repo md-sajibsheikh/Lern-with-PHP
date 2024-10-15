2512116f145d0e948ed0101c942e4151efec322c
mdsajibsheikh <mdsajibsheikh012@gmail.com>
2024-10-15 23:14:10 +0600

    Variables & Syntax

---
+++ Basic PHP Syntax and Variables.php
@@ -0,0 +1,10 @@
+// PHP Installation: Prathome PHP environment setup koro. XAMPP ba WAMP diye local server setup kore PHP scripts run korte paro.
+// Variables: PHP-r variables ($) er upor bhalo dharana koro.
+
+
+<?php
+
+    $name = "John";
+    $age = 25;
+    echo "Name: " . $name . ", Age: " . $age;
+?>
