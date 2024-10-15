db70a1a6a367d5247acb0a05948bb786fcff302c
mdsajibsheikh <mdsajibsheikh012@gmail.com>
2024-10-16 00:06:05 +0600

    Problem 1

--- Basic Problem Solving Ideas.php
+++ Basic Problem Solving Ideas.php
@@ -1,20 +1,20 @@
-
-
-
 Sum of Numbers:
 Kono array theke sokol number jog koro.
+
 Prime Number Check: Kon number prime kina ta check koro.
 Factorial Calculation: Kono number er factorial ber koro.
 Palindrome Check: String palindrom kina check koro.
 Array Sorting: Kono array ke manually sorting koro (ascending ba descending). -->
 
 Practice with Simple Projects
-Mini Calculator: PHP-r maddhome ekta calculator toiri koro jeta basic 
-                addition, subtraction, multiplication, division korte pare.
+Mini Calculator: PHP-r maddhome ekta calculator toiri koro jeta basic
+addition, subtraction, multiplication, division korte pare.
 
-Todo List: PHP-r form submission, data handling diye ekta simple todo list 
-            banate paro jekhane tasks add, edit, ba delete kora jabe.
+
+Todo List: PHP-r form submission, data handling diye ekta simple todo list
+banate paro jekhane tasks add, edit, ba delete kora jabe.
+
 
 Debugging and Error Handling
-                Error detect korte var_dump(), print_r(), ba die() use koro jate code kon part e problem hochhe bujhte paro.
-                PHP diye basic problem solving practice korar shera upay holo chhoto chhoto coding challenges complete kora ebong tarpor step by step boro projects-e kaj kora. Keep practicing!
+Error detect korte var_dump(), print_r(), ba die() use koro jate code kon part e problem hochhe bujhte paro.
+PHP diye basic problem solving practice korar shera upay holo chhoto chhoto coding challenges complete kora ebong tarpor step by step boro projects-e kaj kora. Keep practicing!
